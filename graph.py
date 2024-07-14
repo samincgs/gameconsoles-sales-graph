fe3c07288b3ecd5ea432d9456e0016845550b68e
samin <samincgs@gmail.com>
2024-07-13 21:16:14 -0400

    added different color bars, added a legend, created an annotation for each bar, project is now finished

--- graph.py
+++ graph.py
@@ -1,5 +1,19 @@
 import matplotlib.pyplot as plt
 
+def autolabel(bars):
+    for bar_rect in bars:
+        for rect in bar_rect:
+            width = rect.get_x() + rect.get_width() / 2
+            height = rect.get_height()
+            
+            ax.annotate(
+                text=str(height), 
+                xy=(width, height), 
+                xytext=(0, 3), 
+                textcoords='offset points',  
+                ha='center',
+                color = 'grey')
+
 phases = ['Mid 1990s', 'Early 2k', 'Mid 2k', 'Mid 2010s']
 
 # estimated values (not fully accurate)
@@ -21,17 +35,17 @@ ax.set_title('Game consoles over the years')
 # fix y labels
 ax.set_ylabel('Total sales (in millions)')
 
-
-
 #bar chart
-ax.bar(x_playstation, playstation_sales, width, label= 'Playstation', color = 'slateblue') 
-ax.bar(x_xbox, xbox_sales, width, label = 'Xbox', color = 'springgreen')
-ax.bar(x_nintendo, nintendo_sales, width, label= 'Nintendo', color = 'hotpink')
+playstation_bar = ax.bar(x_playstation, playstation_sales, width, label= 'Playstation', color = 'slateblue') 
+xbox_bar = ax.bar(x_xbox, xbox_sales, width, label = 'Xbox', color = 'springgreen')
+nintendo_bar = ax.bar(x_nintendo, nintendo_sales, width, label= 'Nintendo', color = 'hotpink')
 
 #plot graph
-ax.plot(phases, pc_sales, label = 'PC Sales', color = 'aquamarine')
+ax.plot(phases, pc_sales, label = 'PC Sales', color = 'aquamarine', marker = 'o')
 
 #add a legend
 ax.legend()
+
+autolabel([playstation_bar, xbox_bar, nintendo_bar])
 
 plt.show()
